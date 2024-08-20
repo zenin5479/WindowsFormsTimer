f1a475f3a74434d938685cf37f8f280abd919cf5
zenin5479 <zenin5479@gmail.com>
2024-08-20 22:53:44 +0300

    Update 0.3 Обновлен метод

---
+++ TimerTwo/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TimerTwo
+{
+   partial class Form1
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         ButtonStop = new System.Windows.Forms.Button();
+         ButtonStart = new System.Windows.Forms.Button();
+         TextBoxSeconds = new System.Windows.Forms.TextBox();
+         SuspendLayout();
+         // 
+         // ButtonStop
+         // 
+         ButtonStop.Location = new System.Drawing.Point(81, 50);
+         ButtonStop.Margin = new System.Windows.Forms.Padding(4);
+         ButtonStop.Name = "ButtonStop";
+         ButtonStop.Size = new System.Drawing.Size(60, 32);
+         ButtonStop.TabIndex = 19;
+         ButtonStop.Text = "Стоп";
+         ButtonStop.UseVisualStyleBackColor = true;
+         // 
+         // ButtonStart
+         // 
+         ButtonStart.Location = new System.Drawing.Point(13, 50);
+         ButtonStart.Margin = new System.Windows.Forms.Padding(4);
+         ButtonStart.Name = "ButtonStart";
+         ButtonStart.Size = new System.Drawing.Size(60, 32);
+         ButtonStart.TabIndex = 18;
+         ButtonStart.Text = "Старт";
+         ButtonStart.UseVisualStyleBackColor = true;
+         // 
+         // TextBoxSeconds
+         // 
+         TextBoxSeconds.Location = new System.Drawing.Point(13, 13);
+         TextBoxSeconds.Margin = new System.Windows.Forms.Padding(4);
+         TextBoxSeconds.Name = "TextBoxSeconds";
+         TextBoxSeconds.Size = new System.Drawing.Size(128, 29);
+         TextBoxSeconds.TabIndex = 17;
+         TextBoxSeconds.Text = "0";
+         TextBoxSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+         // 
+         // Form1
+         // 
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         ClientSize = new System.Drawing.Size(326, 450);
+         Controls.Add(ButtonStop);
+         Controls.Add(ButtonStart);
+         Controls.Add(TextBoxSeconds);
+         Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+         Name = "Form1";
+         Text = "Таймер";
+         Load += Form1_Load;
+         ResumeLayout(false);
+         PerformLayout();
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button ButtonStop;
+      private System.Windows.Forms.Button ButtonStart;
+      private System.Windows.Forms.TextBox TextBoxSeconds;
+   }
+}
---
+++ TimerTwo/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+// System.Windows.Forms.Timer
+
+namespace TimerTwo
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
+   }
+}
