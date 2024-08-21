37fdcaa02f7b388fbf9412e6d32db71b4748623f
zenin5479 <zenin5479@gmail.com>
2024-08-21 21:58:44 +0300

    Update 2.3 Обновлен метод

---
+++ TimerFour/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace TimerFour
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void Form1_Load(object sender, System.EventArgs e)
+      {
+
+      }
+   }
+}
