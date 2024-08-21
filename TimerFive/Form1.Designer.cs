df72de54a18296ff1a8bec28f9b5e5124e46a541
zenin5479 <zenin5479@gmail.com>
2024-08-21 22:11:56 +0300

    Update 2.7 Обновлен метод

---
+++ TimerFive/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace TimerFive
+{
+   partial class Form1
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         SuspendLayout();
+         // 
+         // Form1
+         // 
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         ClientSize = new System.Drawing.Size(800, 450);
+         MaximizeBox = false;
+         MinimizeBox = false;
+         Name = "Form1";
+         Text = "Таймер";
+         ResumeLayout(false);
+      }
+
+      #endregion
+   }
+}
